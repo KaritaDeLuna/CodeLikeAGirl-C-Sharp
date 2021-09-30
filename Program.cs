9382c7eb8316d6f5ff9326aa2b14896da35b7c92
KaritaDeLuna <91644399+KaritaDeLuna@users.noreply.github.com>
2021-09-30 20:34:26 +1000

    Add files via upload - final code for lesson 1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson_1_Activity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("And so we've begun upon our road\nOf Learning C#, as we learn to code");   
+        }
+    }
+}
+
+
