c9b46ab2fdb174e309ef6ec8c0f9156979265219
KaritaDeLuna <91644399+KaritaDeLuna@users.noreply.github.com>
2021-10-03 11:42:28 +1100

    Lesson 1

---
+++ Lesson 1 Activity/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson_1_Activity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("And so we've begun upon our road");
+           Console.WriteLine("Of Learning C#, as we learn to code");   
+        }
+    }
+}
+
+
