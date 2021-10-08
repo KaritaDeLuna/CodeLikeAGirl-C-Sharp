d2a4456254f64018712650f7f614bf4d9462c6a2
KaritaDeLuna <91644399+KaritaDeLuna@users.noreply.github.com>
2021-10-08 18:28:06 +1100

    2nd commit - clean up inputs, change output text

--- Lesson 2/MadLibs Assignment/Program.cs
+++ Lesson 2/MadLibs Assignment/Program.cs
@@ -6,23 +6,40 @@ namespace MadLibs_Assignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What's your name?");
+            Console.WriteLine("What's your first name?");
             var rapperName = Console.ReadLine();
+            rapperName = rapperName.Trim();
+            rapperName = rapperName.ToUpper();
+            /*
+            find how to only write first letter of input
+            */
             Console.WriteLine("Favourite day of the week? Please write full day");
             var week = Console.ReadLine();
+            week = week.Trim();
+            /*
+             find how to capitalise only the first letter for this one?
+            */
             Console.WriteLine("Name a coding language you are interested in");
             var langName = Console.ReadLine();
-            Console.WriteLine("Favourite food? If plural end with a 'z', e.g. beanz");
+            langName = langName.Trim();
+            langName = langName.ToUpper();
+            Console.WriteLine("Favourite food? Slang preferred if applicable, e.g. 'choccy' or 'beanz'");
             var yummyFood = Console.ReadLine();
+            yummyFood = yummyFood.Trim();
+            yummyFood = yummyFood.ToLower();
             Console.WriteLine("Who is your favourite writer or poet?");
             var coolPerson = Console.ReadLine();
-            Console.WriteLine("Descriptive word rhyming with 'stay', e.g. gourmet, risqué, okay");
+            coolPerson = coolPerson.Trim();
+            coolPerson = coolPerson.ToUpper();
+            Console.WriteLine("Descriptive word rhyming with 'stay', e.g. gourmet, risqué, blown away");
             var adj1 = Console.ReadLine();
+            adj1 = adj1.Trim();
+            adj1 = adj1.ToUpper();
             Console.WriteLine("\n\nYo yo yo... When i write SEE you write SHARP!\nSEE!");
             var answer1 = Console.ReadLine();
             Console.WriteLine("\nSEE!");
             var answer2 = Console.ReadLine();
-            Console.WriteLine($"\nYo my name is {rapperName} and I'm here to say\nI code like a girl on {week},\nmy interest - {langName}, my style - {adj1},\nI study, have some {yummyFood} then hit the hay!\n\nYo thanks for playing \n...and as {coolPerson} likes to say, keep it fly, fresh and responsible!\n");
+            Console.WriteLine($"\nYo my name is {rapperName} and I'm here to say\nI code like a girl on {week},\nMy interest - {langName}, \nMy style - {adj1},\nOpen VScode up to have a play!\n\nI sit down and study {langName} all day, \nhave a whole lotta {yummyFood} then hit the hay!\n\nThanks for playing \n...and as {coolPerson} likes to say, keep it FLY, FRESH... and RESPONSIBLE!\n");
 
 
          /* 
