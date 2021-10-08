8a8f92de14cd8727dabdb9360c2d0308b12bb4d6
KaritaDeLuna <91644399+KaritaDeLuna@users.noreply.github.com>
2021-10-08 14:46:17 +1100

    First commit with CodeAlong files

---
+++ Lesson 2/CodeAlong - Variables Theory/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lesson_2_Activity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var WelcomeMessage = "Good Evening";
+            var greeting = "How are you today?";
+
+            Console.WriteLine(WelcomeMessage);
+            Console.WriteLine(greeting);
+        }
+    }
+}
---
+++ Lesson 2/CodeAlong Strings + User Input/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodeAlong_Strings___User_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}!");
+        }
+    }
+}
