7838db84daa2e6b2bb0cc66b6d3ea9ef514794c1
KaritaDeLuna <91644399+KaritaDeLuna@users.noreply.github.com>
2021-10-10 14:41:47 +1100

    3rd commit
    more concise
    applied all the extra fns i wanted from tute 2
    haven't looked at formatting yet

--- Lesson 2/MadLibs Assignment/Program.cs
+++ Lesson 2/MadLibs Assignment/Program.cs
@@ -8,47 +8,42 @@ namespace MadLibs_Assignment
         {
             Console.WriteLine("What's your first name?");
             var rapperName = Console.ReadLine();
-            rapperName = rapperName.Trim();
-            rapperName = rapperName.ToUpper();
-            /*
-            find how to only write first letter of input
-            */
+        
             Console.WriteLine("Favourite day of the week? Please write full day");
-            var week = Console.ReadLine();
-            week = week.Trim();
-            /*
-             find how to capitalise only the first letter for this one?
-            */
+            var week = Console.ReadLine().Trim();
+
             Console.WriteLine("Name a coding language you are interested in");
-            var langName = Console.ReadLine();
-            langName = langName.Trim();
-            langName = langName.ToUpper();
+            var langName = Console.ReadLine().Trim().ToUpper();
+
             Console.WriteLine("Favourite food? Slang preferred if applicable, e.g. 'choccy' or 'beanz'");
-            var yummyFood = Console.ReadLine();
-            yummyFood = yummyFood.Trim();
-            yummyFood = yummyFood.ToLower();
+            var yummyFood = Console.ReadLine().Trim().ToLower();
+
             Console.WriteLine("Who is your favourite writer or poet?");
-            var coolPerson = Console.ReadLine();
-            coolPerson = coolPerson.Trim();
-            coolPerson = coolPerson.ToUpper();
-            Console.WriteLine("Descriptive word rhyming with 'stay', e.g. gourmet, risqué, blown away");
-            var adj1 = Console.ReadLine();
-            adj1 = adj1.Trim();
-            adj1 = adj1.ToUpper();
-            Console.WriteLine("\n\nYo yo yo... When i write SEE you write SHARP!\nSEE!");
-            var answer1 = Console.ReadLine();
+            var coolPerson = Console.ReadLine().Trim().ToUpper();
+
+            Console.WriteLine("Descriptive word rhyming with 'stay', e.g. gourmet, okay, passé, doomsday");
+            var adj1 = Console.ReadLine().Trim().ToUpper();
+
+            Console.WriteLine("\n\nYo yo yo... When i write 'SEE' you write 'SHARP'!\n\nSEE!");
+            Console.ReadLine(); //note for later: why define a new variable if u don't care what the input is
+
             Console.WriteLine("\nSEE!");
-            var answer2 = Console.ReadLine();
-            Console.WriteLine($"\nYo my name is {rapperName} and I'm here to say\nI code like a girl on {week},\nMy interest - {langName}, \nMy style - {adj1},\nOpen VScode up to have a play!\n\nI sit down and study {langName} all day, \nhave a whole lotta {yummyFood} then hit the hay!\n\nThanks for playing \n...and as {coolPerson} likes to say, keep it FLY, FRESH... and RESPONSIBLE!\n");
+            Console.ReadLine(); 
+
+            Console.WriteLine($"\nYo my name's BIG {(char.ToUpper(rapperName[0]))} and I'm here to say\nI code like a girl on {(char.ToUpper(week[0]) + week.Substring(1))},\nMy interest - {langName}, \nMy style - {adj1},\nOpen VScode up to have a play!\n\nI sit down and study {langName} all day, \nHave a whole lotta {yummyFood} then hit the hay!\n\n...Thanks for playing \nAnd as {coolPerson} likes to say, keep it FLY, FRESH... and RESPONSIBLE!\n");
 
 
          /* 
          Rap lyric generator
          I used \n rather than new WriteLines here to be purposely unclear about the output to anybody reading the script.
 
-         for me: check and run AS YOU GO! 
-         ... and ask about bool output in tute
+         NOTES FOR LATER: 
+         check and run AS YOU GO! 
+         actually,new lines (i.e. Console.Write[Line]) mo'betta for error troubleshooting
+         sus out formatting conventions/extensions
+         and ask about bool output in tute
          */
+            
         }
     }
 }
